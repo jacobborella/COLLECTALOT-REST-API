43eea4da8da8a54ae6fb3b3645f0c10255f0a65c
Jacob Borella <jacobborella@hotmail.com>
2017-05-05 10:01:55 +0200

    changed version

--- src/main/java/org/collectalot/service/TitlePathService.java
+++ src/main/java/org/collectalot/service/TitlePathService.java
@@ -1,6 +1,7 @@
 package org.collectalot.service;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,6 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.collectalot.model.TitlePath;
 
 @Controller
+@Configuration
 @RequestMapping("/title-path")
 public class TitlePathService {
 	
