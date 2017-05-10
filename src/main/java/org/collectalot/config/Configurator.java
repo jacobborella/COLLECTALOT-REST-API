b1d126f592ce9f6919b7acb2e42c36824d020092
Jacob Borella <jacobborella@hotmail.com>
2017-05-11 00:37:17 +0200

    config in spring-config.xml

--- src/main/java/org/collectalot/config/Configurator.java
+++ src/main/java/org/collectalot/config/Configurator.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication(scanBasePackages={"org.collectalot.service"})
 @Configuration
-@ImportResource("classpath:resources/spring-config.xml")
+@ImportResource("file:src/main/java/resources/spring-config.xml")
 public class Configurator {
     public static void main(String[] args) {
         SpringApplication.run(Configurator.class, args);
