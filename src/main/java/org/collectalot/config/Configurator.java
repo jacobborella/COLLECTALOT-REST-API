e74f8d0f15317469ded4609efa367e8f00400108
Jacob Borella <jacobborella@hotmail.com>
2017-05-11 00:21:19 +0200

    config in spring-config.xml

--- src/main/java/org/collectalot/config/Configurator.java
+++ src/main/java/org/collectalot/config/Configurator.java
@@ -1,18 +1,15 @@
 package org.collectalot.config;
 
-import org.collectalot.dao.TitlePartDAO;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication(scanBasePackages={"org.collectalot.service"})
-
+@Configuration
+@ImportResource("classpath:spring-config.xml")
 public class Configurator {
     public static void main(String[] args) {
         SpringApplication.run(Configurator.class, args);
-    }
-    @Bean
-    public TitlePartDAO titlePartDAO() {
-    	return new TitlePartDAO();
     }
 }
