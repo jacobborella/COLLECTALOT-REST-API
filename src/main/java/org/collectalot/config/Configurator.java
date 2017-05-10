b38c6518aee76560178f8c64c72c3e6aa194b76f
Jacob Borella <jacobborella@hotmail.com>
2017-05-11 00:53:52 +0200

    config in spring-config.xml

--- src/main/java/org/collectalot/config/Configurator.java
+++ src/main/java/org/collectalot/config/Configurator.java
@@ -1,6 +1,9 @@
 package org.collectalot.config;
 
 import org.collectalot.dao.TitlePartDAO;
+import org.collectalot.dao.TitlePartDAOMock;
+import org.collectalot.dao.TitlePartInterface;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -9,11 +12,17 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication(scanBasePackages={"org.collectalot.service"})
 //@ImportResource("classpath:spring-config.xml")
 public class Configurator {
+	@Value("${ENV}")
+	private String env;
     public static void main(String[] args) {
         SpringApplication.run(Configurator.class, args);
     }
     @Bean
-    public TitlePartDAO titlePartDAO() {
-    	return new TitlePartDAO();
+    public TitlePartInterface titlePartDAO() {
+    	if("dev".equals(env)) {
+    		return new TitlePartDAOMock();
+    	} else {
+    		return new TitlePartDAO();
+    	}
     }
 }
